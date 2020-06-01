c8f3d38f0308e37b697a16cc063d08f772762853
Abbas Rzayev <rzayevabbas22@gmail.com>
2020-06-01 12:06:29 +0400

    izmenenie

---
+++ NewCalculateTest/UnitTest1.cs
@@ -0,0 +1,72 @@
+﻿using New_Calculate;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace NewCalculateTest
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData(3, 2, 5)]
+        [InlineData(4, 2, 6)]
+        [InlineData(10, 20, 30)]
+
+        public void TestSum(double a, double b, double expected)
+        {
+            SimpleCalculate Calc = new SimpleCalculate();
+
+            double actual = Calc.Sum(a, b);
+            Assert.Equal(expected, actual);
+        }
+        [Theory]
+        [InlineData(3, 2, 1)]
+        [InlineData(4, 2, 2)]
+        [InlineData(20, 10, 10)]
+
+        public void TestMin(double a, double b, double expected)
+        {
+            SimpleCalculate Calc = new SimpleCalculate();
+
+            double actual = Calc.Min(a, b);
+            Assert.Equal(expected, actual);
+        }
+        [Theory]
+        [InlineData(3, 2, 6)]
+        [InlineData(4, 2, 8)]
+        [InlineData(10, 20, 200)]
+
+        public void TestMult(double a, double b, double expected)
+        {
+            SimpleCalculate Calc = new SimpleCalculate();
+
+            double actual = Calc.Mult(a, b);
+            Assert.Equal(expected, actual);
+        }
+        [Theory]
+        [InlineData(6, 2, 3)]
+        [InlineData(4, 2, 2)]
+        [InlineData(20, 10, 2)]
+
+        public void Div (double a, double b, double expected)
+        {
+            SimpleCalculate Calc = new SimpleCalculate();
+
+            double actual = Calc.Div(a, b);
+            Assert.Equal(expected, actual);
+        }
+        [Theory]
+        [InlineData(121,11 )]
+        [InlineData(144,12)]
+        [InlineData(225,15 )]
+
+        public void TestSqrt(double a, double expected)
+        {
+            SimpleCalculate Calc = new SimpleCalculate();
+
+            double actual = Calc.Sqrt(a);
+            Assert.Equal(expected, actual);
+        }
+
+    }
+}
---
+++ New_Calculate/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New_Calculate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleCalculate Calc = new SimpleCalculate();
+
+            while(true)
+            {
+                Console.Clear();
+                Console.WriteLine("Choose on of the operation : \n 1. addition  \n 2. subtraction" +
+                    " \n 3. multiplication \n 4. division \n 5. square root     ");
+
+                switch(Console.ReadLine())
+                {
+                    case "1":
+                        try
+                        {
+                            Calc.Menyu();
+                            Console.WriteLine($"Result:{Calc.Sum(Calc.a, Calc.b)}");
+                        }
+                        catch (Exception exp)
+                        {
+                            Console.WriteLine($"Error{exp.Message}");
+                        }
+                        break;
+                    case "2":
+                        try
+                        {
+                            Calc.Menyu();
+                            Console.WriteLine($"Result:{Calc.Min(Calc.a, Calc.b)}");
+                        }
+                        catch (Exception exp)
+                        {
+                            Console.WriteLine($"Error{exp.Message}");
+                        }
+                        break;
+                    case "3":
+                        try
+                        {
+                            Calc.Menyu();
+                            Console.WriteLine($"Result:{Calc.Mult(Calc.a, Calc.b)}");
+                        }
+                        catch (Exception exp)
+                        {
+                            Console.WriteLine($"Error{exp.Message}");
+                        }
+                        break;
+                    case "4":
+                        try
+                        {
+                            Calc.Menyu();
+                            Console.WriteLine($"Result:{Calc.Div(Calc.a, Calc.b)}");
+                        }
+                        catch (Exception exp)
+                        {
+                            Console.WriteLine($"Error{exp.Message}");
+                        }
+                        break;
+                    case "5":
+                        try
+                        {
+                            Console.WriteLine("Введите число");
+                            Calc.a = double.Parse(Console.ReadLine());
+                            Console.WriteLine($"Result: {Calc.Sqrt(Calc.a)}");
+                        }
+                        catch (Exception Exp)
+                        {
+                            Console.WriteLine($"Error {Exp.Message}");
+                        }
+                        break;
+
+                       
+                }
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
